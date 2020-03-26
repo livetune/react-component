a3add50c2af5bb084441b6f7d669d826da705917
livetune <243443394@qq.com>
2020-03-27 01:29:23 +0800

    更换路由模式

--- src/router/router.js
+++ src/router/router.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import { Route, HashRouter, Switch } from 'react-router-dom';
 // import asyncComponent from '../asyncComponent';
 import cardScroll from '../example/cardSlide/index';
 import dialog from '../example/dialog/index';
 
 const Root = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Switch>
       <Route path="/" component={cardScroll} exact />
       <Route path="/cardSlide" component={cardScroll} />
       <Route path="/dialog" component={dialog} />
     </Switch>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default Root;
