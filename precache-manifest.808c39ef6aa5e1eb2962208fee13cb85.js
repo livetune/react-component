2ce831f1c8393e53c7c9daa025046e17b66cf133
livetune <243443394@qq.com>
2020-03-27 01:24:07 +0800

    clean

---
+++ precache-manifest.808c39ef6aa5e1eb2962208fee13cb85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9694ac9b141148a18def",
+    "url": "./static/css/main.be4333b0.chunk.css"
+  },
+  {
+    "revision": "9694ac9b141148a18def",
+    "url": "./static/js/main.9694ac9b.chunk.js"
+  },
+  {
+    "revision": "96231b9889521310c763",
+    "url": "./static/js/1.96231b98.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "099779cd3848d72386bd8d76f9269f7e",
+    "url": "./static/media/user.099779cd.svg"
+  },
+  {
+    "revision": "55650d4b7d781917181fcf227ff704f5",
+    "url": "./index.html"
+  }
+];
